1c7f5acdff571c7b932fadbd830b5d15339bd112
ukari <muromi.ukari@gmail.com>
2016-04-14 11:05:34 +0800

    fix bug, like [Object]

--- js/init.js
+++ js/init.js
@@ -1,4 +1,5 @@
 var hide_other_containers = regist_container_hide_controller(["article_lists", "article_container"]);
+var hide_other_containers_by_id = function (id) {hide_other_containers(document.getElementById(id));};
 base_request("GET", "http://"+window.location.host+"/config.json", undefined, load_config, document.getElementById("notification"));
 
 function load_config(data, target)
