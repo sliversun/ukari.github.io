ebf38398d065a02c2135c3b05cc87f027f6bba6d
ukari <muromi.ukari@gmail.com>
2016-04-15 05:41:29 +0800

    developing oauth feature

--- js/request.js
+++ js/request.js
@@ -50,7 +50,7 @@ function base_request(method, url, data, processfn, target)
   }
   var xhttp=new XMLHttpRequest();
   xhttp.open(method, dsturl, true);
-  xhttp.header("Access-Control-Allow-Origin", "*");
+  xhttp..setRequestHeader("Access-Control-Allow-Origin", "*");
   console.log("dst: "+dsturl);
   xhttp.onreadystatechange=function ()
   {
